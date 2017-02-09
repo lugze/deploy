2790e78fd8125e7b2ae63ca1ca2b3ada27796696
Ned <ned@Neds-MacBook-Pro.local>
2017-02-09 20:48:29 +0100

    change username

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -2,5 +2,5 @@ set :stage, :development
 set :branch, "development"
 set :deploy_to, "/var/www/lugze.org/#{fetch(:branch)}"
 
-server 'lugze_server', user: 'lugze_dev', roles: %w{web app}, port: 22115
+server 'lugze_server', user: 'lugze', roles: %w{web app}, port: 22115
 
