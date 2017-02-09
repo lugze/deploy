43d3bce4a0acfaa78e93fa6c58ca1a9f56c73e23
Ned <ned@Neds-MacBook-Pro.local>
2017-02-09 20:46:38 +0100

    sada radu multi stage deploy

--- config/deploy/dev.rb
+++ config/deploy/dev.rb
@@ -1,4 +1,6 @@
 set :stage, :dev
 set :branch, "dev"
+set :deploy_to, "/var/www/lugze.org/#{fetch(:branch)}"
+
 server 'lugze_server', user: 'lugze', roles: %w{web app}, port: 22115
 
--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,4 +1,6 @@
 set :stage, :development
 set :branch, "development"
+set :deploy_to, "/var/www/lugze.org/#{fetch(:branch)}"
+
 server 'lugze_server', user: 'lugze_dev', roles: %w{web app}, port: 22115
 
