6c5616654e864ea15fbaec41989a381dfe1fcb4b
Nedim Hadzimahmutovic <nedim@Nedims-Mac-Pro.local>
2017-02-10 13:40:33 +0100

    hardkodirao deploy_to path za development

--- config/deploy/development.rb
+++ config/deploy/development.rb
@@ -1,6 +1,6 @@
 set :stage, :development
 set :branch, "master"
-set :deploy_to, "/var/www/lugze.org/#{fetch(:branch)}"
+set :deploy_to, "/var/www/lugze.org/development"
 set :repo_url, "git@github.com:lugze/new_site.git"
 
 server 'lugze_server', user: 'lugze', roles: %w{web app}, port: 22115
